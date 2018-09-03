b309ef07f7c2c29cd0ed470675782153f3030891
DESKTOP-U1J0KEJ\ROG <463736788@qq.com>
2018-09-03 15:15:37 +0800

    2018-09-03

---
+++ VUE/dome02/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{msg}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  msg = '张萌你这个吊毛！'
+  mounted () {
+    this.init()
+  }
+  init(){
+    console.log('hahaha')
+  }
+}
+</script>
