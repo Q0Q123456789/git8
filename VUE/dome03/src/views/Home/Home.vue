fc59176b03c3c5b24e3029dc52f2430a2ec5be79
464633053 <463736788@qq.com>
2018-10-22 18:00:38 +0800

    2018-10-22

--- VUE/dome03/src/views/Home/Home.vue
+++ VUE/dome03/src/views/Home/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Row :gutter="16" class="code-row-bg">
+      <template v-for="item in 7">
+        <Col span="6" class="code-col-padbottom"> 
+          <Card >
+              <p slot="title">Disable card with hover shadows</p>
+              <p>Content of card</p>
+              <p>Content of card</p>
+              <p>Content of card</p>
+          </Card>
+        </Col>
+      </template>
+    </Row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home_page',
+  name: "home_page",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+<style lang='less'>
+  .home{
+    .code-col-padbottom{
+      padding-bottom: 16px;
+    }
+  }
+</style>
