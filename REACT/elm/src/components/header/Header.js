df4fc829d41bab214e3b0532e74eb70f30e7abee
wx463736788 <wx463736788@outlook.com>
2019-05-20 11:34:36 +0800

    2019

--- REACT/elm/src/components/header/Header.js
+++ REACT/elm/src/components/header/Header.js
@@ -1,10 +1,19 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
+// import { StackNavigator, TabNavigator } from "react-navigation";
+// import SignatureView from "react-native-signature";
+// import Signature from 'react-native-signature-canvas';
 
 class Header extends Component {
   render() {
-    return ( <div className = "header" >header; </div>
+    return ( <div className = "header" >header; 
+    {/* <SignatureView></SignatureView> */}
+      {/* <Signature 
+        // onOK={(img) => console.log(img)} 
+        // descriptionText="Sign" 
+        // clearText="Clear" 
+        // confirmText="Save" 
+        webStyle={`.m-signature-pad--footer .button {  background-color: red; color: #FFF; }`} /> */}
+    </div>
     );
   }
 }
